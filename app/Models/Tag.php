bd21afcf17242d9a8a29ad52b22c853dff1d68fc
Lordwill1 <messagelordwill@gmail.com>
2024-07-27 01:12:41 +0100

    Pivot Tables and BelongsToMany Relationships

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -11,6 +11,7 @@ class Tag extends Model
 
     public function jobs()
     {
-        return $this->belongsToMany(Job::class);
+        return $this->belongsToMany(Job::class, 'job_tags', 'tag_id', 'job_listing_id');
     }
 }
+ 
